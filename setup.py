8db5259a167f3c1bb4a3d9de696a2fcfeabcfba0
erik_ritter <erik.ritter@airbnb.com>
2019-07-08 09:14:09 -0700

    Upgrade FAB to 2.1.6

--- setup.py
+++ setup.py
@@ -77,8 +77,7 @@ setup(
         'croniter>=0.3.28',
         'cryptography>=2.4.2',
         'flask>=1.0.0, <2.0.0',
-        'flask-appbuilder @ git+git://github.com/dpgaspar/Flask-AppBuilder' +
-        '@93d3583192667e11f121a52d9c9a1342a2454d4a#egg=flask-appbuilder',
+        'flask-appbuilder>=2.1.6, <2.3.0',
         'flask-caching',
         'flask-compress',
         'flask-talisman',
