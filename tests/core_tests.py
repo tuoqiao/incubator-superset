73e0f1c46587acf0013458f35f7dbd911115d719
John Bodley <4567245+john-bodley@users.noreply.github.com>
2020-01-27 22:28:42 -0800

    [fix] Reverting metic logic from #8901 (#9030)
    (cherry picked from commit dc60db2a433e7ac04c40f8e9fe43617717ebb7d8)

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -108,7 +108,7 @@ class CoreTests(SupersetTestCase):
                 {
                     "granularity": "ds",
                     "groupby": ["name"],
-                    "metrics": ["sum__num"],
+                    "metrics": [{"label": "sum__num"}],
                     "filters": [],
                     "row_limit": 100,
                 }
